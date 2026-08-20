ee022de0692ea79fdf8b5b3a4633b63eee7f4b8a
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-08-20 23:37:28 +0800

    Fix Compose smoke test interaction import

--- app/src/androidTest/java/io/github/z121z1/watermarkcleaner/AppSmokeTest.kt
+++ app/src/androidTest/java/io/github/z121z1/watermarkcleaner/AppSmokeTest.kt
@@ -3,6 +3,7 @@ package io.github.z121z1.watermarkcleaner
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
