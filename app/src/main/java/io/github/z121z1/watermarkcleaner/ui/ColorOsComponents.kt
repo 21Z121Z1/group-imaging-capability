4993e968387cb2bb2ae1414fe6828f3d9f11bafe
21Z121Z1 <89170834+21Z121Z1@users.noreply.github.com>
2026-08-21 14:38:33 +0800

    Use BoxScope matchParentSize directly

--- app/src/main/java/io/github/z121z1/watermarkcleaner/ui/ColorOsComponents.kt
+++ app/src/main/java/io/github/z121z1/watermarkcleaner/ui/ColorOsComponents.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
